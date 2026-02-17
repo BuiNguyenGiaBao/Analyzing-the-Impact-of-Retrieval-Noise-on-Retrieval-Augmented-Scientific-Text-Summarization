92f676927be9bbc6c2e3ab65a33820f03afceb0e
Bùi Nguyễn Gia Bảo <162402329+BuiNguyenGiaBao@users.noreply.github.com>
2026-02-17 19:45:16 +0700

    1

--- retrivel.py
+++ retrivel.py
@@ -1,4 +1,4 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
+from typing import List, Dict, Any, Tuple
+import numpy as np
+import faiss
+from sentence_transformers import SentenceTransformer
